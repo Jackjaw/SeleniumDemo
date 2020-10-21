89f9084c471eb4ee2bb10ef4c58a5e39c806a265
Jackjaw <shdongwen@gmail.com>
2020-10-21 17:17:34 +0800

    add test case

---
+++ test/test-case-4.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { GetDriver } = require('./util/driver');
+
+describe('case-4', function () {
+    this.timeout(10000);
+    let driver;
+    beforeEach(async () => {
+        this.timeout(5000);
+        driver = this.driver = await GetDriver('http://localhost:3000');
+    });
+    it('event-1', function () {
+        assert.ok(false);
+    });
+});
